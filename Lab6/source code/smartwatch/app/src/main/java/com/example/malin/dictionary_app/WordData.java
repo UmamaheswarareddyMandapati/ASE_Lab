9163520a0e0ed57d61cd83991179f798ea5d196b
UmamaheswarareddyMandapati <umamaheshreddy100@gmail.com>
2017-03-01 19:36:59 -0600

    Lab6

---
+++ Lab6/source code/smartwatch/app/src/main/java/com/example/malin/dictionary_app/WordData.java
@@ -0,0 +1,32 @@
+package com.example.malin.dictionary_app;
+
+
+public class WordData {
+    String zipf;
+    String perMillion;
+    String diversity;
+
+    public String getDiversity() {
+        return diversity;
+    }
+
+    public void setDiversity(String diversity) {
+        this.diversity = diversity;
+    }
+
+    public String getPerMillion() {
+        return perMillion;
+    }
+
+    public void setPerMillion(String perMillion) {
+        this.perMillion = perMillion;
+    }
+
+    public String getZipf() {
+        return zipf;
+    }
+
+    public void setZipf(String zipf) {
+        this.zipf = zipf;
+    }
+}
